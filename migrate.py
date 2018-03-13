928f5035355bc9dd72ba334372129b92384aeca2
Derek Thomas <sisyphus192@gmail.com>
2018-03-13 13:55:12 -0600

    attempting heroku integration

--- migrate.py
+++ migrate.py
@@ -1,10 +1,13 @@
-import MySQLdb
-import pandas as pd
 from sqlalchemy import create_engine
+import pandas as pd
+import subprocess
 
-engine = create_engine("mysql://root:@localhost:3306/adrestiaDB")
-
+proc = subprocess.Popen('heroku config:get DATABASE_URL -a adrestia', stdout=subprocess.PIPE, shell=True)
+db_url = proc.stdout.read().decode('utf-8').strip() + '?sslmode=require'
+db_url = db_url[:8]+'+psycopg2'+db_url[8:]
+print(db_url)
+engine = create_engine(db_url)
 df = pd.read_csv('data/APPM.csv')
-df.to_sql(con=engine, name='courses', if_exists='replace', index=False)
+df.to_sql(con=engine, name='appm', if_exists='replace', index=True)
 
 
