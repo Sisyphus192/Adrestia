2fa1dcbc8c02a672acdecbbd84d08bfe1f779f24
Derek Thomas <sisyphus192@gmail.com>
2018-04-03 14:45:48 -0600

    fixing something for heroku

--- website/website/urls.py
+++ website/website/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from main.views import course, FCQloginpage, createAccount
+from django.conf.urls import url
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', FCQloginpage),
+    url(r'^$', FCQloginpage),
     path('course/', course),
 #    path('main/', include('main.urls')),
     path('createAccount/', createAccount)
