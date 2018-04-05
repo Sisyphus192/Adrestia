68bc7bf1a0e2e03654238f858349ebcd7c686a2e
Derek Thomas <sisyphus192@gmail.com>
2018-04-05 15:17:35 -0600

    fix for signup

--- website/main/views.py
+++ website/main/views.py
@@ -30,7 +30,7 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username = username, password = raw_password)
             login(request, user)
-            return redirect('home')
+            return redirect('')
     else:
         form = SignUpForm()
     return render(request, 'main/signup.html', {'form':form})
