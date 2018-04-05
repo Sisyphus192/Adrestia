e70afef73dcd26d1be6347a34fe71577425d83fd
Long <long1685@colorado.edu>
2018-04-05 16:37:04 -0600

    changed migrate.py for databases

--- migrate.py
+++ migrate.py
@@ -5,14 +5,14 @@ import psycopg2
 import os
 
 proc = subprocess.Popen('heroku config:get DATABASE_URL -a adrestia', stdout=subprocess.PIPE, shell=True)
-db_url = proc.stdout.read().decode('utf-8').strip() + 'sslmode=require'
-#db_url = db_url[:8]+'+psycopg2'+db_url[8:]
+db_url = proc.stdout.read().decode('utf-8').strip() + '?sslmode=require'
+db_url = db_url[:8]+'ql'+db_url[8:]
 #DATABASE_URL = os.environ['DATABASE_URL']
 #print(DATABASE_URL)
 #print(db_url)
-conn = psycopg2.connect(db_url)
-#engine = create_engine('mysq://root:@localhost:3308/adrestriaDB')
-#df = pd.read_csv('data/APPM.csv')
-#df.to_sql(con=engine, name='courses', if_exists='replace', index=True)
-
-
+#conn = psycopg2.connect(db_url)
+engine = create_engine(db_url)
+df = pd.read_csv('data/APPM.csv')
+print('hello')
+df[:10].to_sql(con=engine, name='courses', if_exists='replace', index=True)
+print('hello2')
