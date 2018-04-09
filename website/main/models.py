43e36213ad51bbece7a7178b4a86669ce1cd14c4
Derek Thomas <sisyphus192@gmail.com>
2018-04-09 09:24:08 -0600

    tweaking model.py

--- website/main/models.py
+++ website/main/models.py
@@ -15,7 +15,6 @@ class Courses(models.Model):
     challenge = models.FloatField(db_column='Challenge', blank=True, null=True)  # Field name made lowercase.
     howmuchlearned = models.FloatField(db_column='HowMuchLearned', blank=True, null=True)  # Field name made lowercase.
     crstitle = models.TextField(db_column='CrsTitle', blank=True, null=True)  # Field name made lowercase.
-    crse_id = models.IntegerField(primary_key=True)
 
     class Meta:
         managed = False
