790b86137cbec31a6126c439857d800e64c0f65b
Derek Thomas <sisyphus192@gmail.com>
2018-04-24 17:34:21 -0600

    attempting to implement django form

--- website/main/views.py
+++ website/main/views.py
@@ -37,7 +37,7 @@ def signup(request):
             raw_password = form.cleaned_data.get('password1')
             user = authenticate(username = username, password = raw_password)
             login(request, user)
-            return redirect('/')
+            return redirect('')
     else:
         form = SignUpForm()
     return render(request, 'main/signup.html', {'form':form})
