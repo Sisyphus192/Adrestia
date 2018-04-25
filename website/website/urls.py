1dc376c026d4c76a5f3ecd252be0c68a2d3efdae
Derek Thomas <sisyphus192@gmail.com>
2018-04-25 14:58:44 -0600

    attempting to implement django form

--- website/website/urls.py
+++ website/website/urls.py
@@ -14,7 +14,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include, url
+from django.urls import path, include
+from django.conf.urls import url
 import main.views
 from django.contrib.auth import views as auth_views
 
